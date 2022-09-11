24f9954ffd904819a7bacfa2e4330426889a408d
Seyed-alireza <ali.r.salehi99@gmail.com>
2022-09-12 00:40:02 +0430

    change some file

--- accounts/models.py
+++ accounts/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    address = models.TextField()
-    phone_number = models.CharField(max_length=11)
 
     def __str__(self):
         return self.username
