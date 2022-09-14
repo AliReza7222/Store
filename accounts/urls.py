45b2db0bba9a1f9dbabe881966e925b0996d174c
Seyed-alireza <ali.r.salehi99@gmail.com>
2022-09-14 19:57:57 +0430

    create a form for edit profile

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateProfile
+from .views import CreateProfile, UpdateProfile
 
 urlpatterns = [
     path('profile/', CreateProfile.as_view(), name='profile'),
+    path('profile/update/<int:pk>/', UpdateProfile.as_view(), name='update-profile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,13 +1,13 @@
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.contrib import messages
-from .models import User
+from .models import User, Profile
 from .forms import UserProfile
 
 
 class CreateProfile(LoginRequiredMixin, CreateView):
-    model = User
+    model = Profile
     template_name = 'profile.html'
     form_class = UserProfile
     login_url = '/accounts/login/'
@@ -16,10 +16,22 @@ class CreateProfile(LoginRequiredMixin, CreateView):
     def post(self, request, *args, **kwargs):
         form = self.form_class(request.POST)
         user = request.user
-        form.instance.user = user
+        profile_exists = Profile.objects.filter(user_id=user.id).exists()
         if form.is_valid():
-            form.save()
-            messages.success(request, 'Your profile has been created successfully . ')
+            Profile.objects.update_or_create(user_id=user.id, defaults=form.cleaned_data)
+            if not profile_exists:
+                message = 'Your profile has been created successfully . '
+            elif profile_exists:
+                message = 'Your profile has been edited successfully . '
+            messages.success(request, message)
             return redirect('profile')
         return render(request, self.template_name, context={'form': form})
 
+
+class UpdateProfile(LoginRequiredMixin, UpdateView):
+    login_url = '/accounts/login/'
+    model = Profile
+    fields = '__all__'
+    template_name = 'profile.html'
+    extra_context = {'update': True}
+    success_url = '/accounts/profile/'
