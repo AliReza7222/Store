b2b5e09bf8de3f2c492b7a0da13c8c9e4f23502b
AliReza7222 <ali.r.salehi99@gmail.com>
2023-07-19 01:14:03 +0430

    view receipt bought and receipt sold created

--- pages/urls.py
+++ pages/urls.py
@@ -5,6 +5,7 @@ from .views import (
     PaymentSimulator,
     SendCode,
     ListBoughtReceipt,
+    ListSoldReceipt
      )
 
 
@@ -14,4 +15,5 @@ urlpatterns = [
     path('payment/', PaymentSimulator.as_view(), name='payment_simulator'),
     path('payment/get_code/', SendCode.as_view(), name='get_code'),
     path('bought_receipt/', ListBoughtReceipt.as_view(), name='bought_receipt'),
+    path('sell_receipt/', ListSoldReceipt.as_view(), name='sell_receipt')
 ]
