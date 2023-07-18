3ee8bd64672a4fcbd8a1c9fba46deb19ee008a0a
AliReza7222 <ali.r.salehi99@gmail.com>
2023-07-19 01:10:23 +0430

    file admin.py for app accounts updated

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from .models import Profile
 
 
 @admin.register(get_user_model())
@@ -15,3 +16,6 @@ class AdminUser(admin.ModelAdmin):
     filter_horizontal = ['groups', 'user_permissions']
     list_display = ['username', 'email', 'first_name', 'last_name', 'is_staff', 'is_active']
     list_editable = ['is_staff', 'is_active']
+
+
+admin.site.register(Profile)
